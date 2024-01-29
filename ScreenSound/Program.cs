3a474b23447040a2ddb7eb558d1e8c101396349c
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-01-28 22:26:23 -0300

    Main (#2)
    * finish
    
    * new commit
    
    * teste

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ScreenSound 
+string mensagemBoasVindas = "Boas vindas ao Screen Sound.";
+Console.WriteLine(mensagemBoasVindas);
+
+// Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+// Mostrar o conteúdo da variável curso no console.
+// Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+// Mostrar na tela o nome dos instrutores do curso.
+
+string curso = "C#: criando sua primeira aplicação C#: criando sua primeira aplicação";
+Console.WriteLine(curso);
+string nome = "Tiago";
+string sobrenome = "Nobre";
+Console.WriteLine(nome);
+Console.WriteLine(sobrenome);
+Console.WriteLine("Instrutores do curso: Guilherme e Daniel Portugal.");  
+//test
