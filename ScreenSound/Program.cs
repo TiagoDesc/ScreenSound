92a2f02ae71a9431d5668019d7b3594c9e491eab
TiagoDesc <nobretiago96@gmail.com>
2024-01-28 22:19:05 -0300

    new commit

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,3 +1,16 @@
 ﻿// ScreenSound 
 string mensagemBoasVindas = "Boas vindas ao Screen Sound.";
 Console.WriteLine(mensagemBoasVindas);
+
+// Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
+// Mostrar o conteúdo da variável curso no console.
+// Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
+// Mostrar na tela o nome dos instrutores do curso.
+
+string curso = "C#: criando sua primeira aplicação C#: criando sua primeira aplicação";
+Console.WriteLine(curso);
+string nome = "Tiago";
+string sobrenome = "Nobre";
+Console.WriteLine(nome);
+Console.WriteLine(sobrenome);
+Console.WriteLine("Instrutores do curso: Guilherme e Daniel Portugal.");  
