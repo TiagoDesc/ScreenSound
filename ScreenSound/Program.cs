304f26941ba8ed372b1ca1e1524d42f6e1ee9887
TiagoDesc <nobretiago96@gmail.com>
2024-01-31 00:46:28 -0300

    list

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -53,15 +53,3 @@ void ExibirOpcoesMenu()
 ExibirMensagem();
 ExibirOpcoesMenu();
 
-// Criar uma variável chamada curso, e guarde nela o nome do curso estudado.
-// Mostrar o conteúdo da variável curso no console.
-// Criar uma variável chamada nome e outra chamada sobrenome e guardar nelas seu nome e sobrenome.
-// Mostrar na tela o nome dos instrutores do curso.
-
-Console.WriteLine("************************************");
-string curso = "C#: criando sua primeira aplicação C#: criando sua primeira aplicação";
-Console.WriteLine(curso);
-string nome = "Tiago";
-string sobrenome = "Nobre.";
-Console.WriteLine(nome + " " + sobrenome);
-Console.WriteLine("Instrutores do curso: Guilherme Lima e Daniel Portugal.");  
