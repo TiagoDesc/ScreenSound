dc29c18adff7a8965dcd10a3a60024a85801a4bb
TiagoDesc <nobretiago96@gmail.com>
2024-02-01 23:52:39 -0300

    att

---
+++ Atividades/Atividade3/Atividade3/Program.cs
@@ -0,0 +1,20 @@
+﻿do
+{
+    int secretNumber = 42;
+    Console.Write("Enter the number between 1 and 100: ");
+    int number = int.Parse(console.readLine());
+
+    if (number == secretNumber){
+        Console.WriteLine("Congratulations, you got the number right!");
+        break;
+    }else if (number < secretNumber){
+        Console.WriteLine("The number that you entered is smaller than the secret number.");
+    }else{
+        Console.WriteLine("The number that you entered is bigger than the secret number.");
+    }
+
+} while (true);
+
+// Aleatory mode: 
+// Random randomNumber = new Random();
+// int secretNumber = randomm.Next(1, 101);
