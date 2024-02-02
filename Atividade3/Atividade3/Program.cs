21e9732996b72ce96904046737cf2b9bf67885c1
TiagoDesc <nobretiago96@gmail.com>
2024-02-01 22:59:20 -0300

    new commit

---
+++ Atividade3/Atividade3/Program.cs
@@ -0,0 +1,16 @@
+﻿do
+{
+    int secretNumber = 42;
+    Console.Write("Enter the number between 1 and 100: ");
+    int number = int.Parse(console.readLine());
+
+    if (number == secretNumber){
+        Console.WriteLine("Congratulations, you got the number right!");
+        break;
+    }else if (number < secretNumber){
+        Console.WriteLine("The number that you entered is smaller than the secret number.");
+    }else{
+        Console.WriteLine("The number that you entered is bigger than the secret number.");
+    }
+
+} while (true);
