7ff2d8d5492cf8192c3edd6588bfc26174ef7760
TiagoDesc <nobretiago96@gmail.com>
2024-02-02 21:22:31 -0300

    new task

---
+++ Atividade4/Atividade4/Program.cs
@@ -0,0 +1,56 @@
+﻿// 1 - Escrever uma função que a partir de dois números de ponto flutuante a e b exiba no console o resultado de suas quatro operações básicas (adição, subtração, divisão e multiplicação), utilizando interpolação de strings.
+
+// 2 - Criar uma lista de bandas vazia e adicionar suas bandas prediletas em seguida.
+
+// 3 - Utilizar a estrutura 'for' para mostrar todas as suas bandas preferidas, listadas na lista do exercício anterior, no console.
+
+// 4 - Criar um programa que calcula a soma de todos os elementos inteiros em uma lista.
+
+
+void Calculo()
+{
+    double a = 10;
+    float b = 2;
+
+    double soma = a + b;
+    double subtracao = a - b;
+    double multiplicacao = a * b;
+    double divisao = a / b;
+
+    Console.WriteLine($"a + b = {soma}");
+    Console.WriteLine($"a - b = {subtracao}");
+    Console.WriteLine($"a * b = {multiplicacao}");
+    Console.WriteLine($"a / b = {divisao}");    
+    Console.WriteLine("***********************");
+}
+
+Calculo();
+
+List<string > banda = new List<string>();
+banda.Add("Onze: 20");
+banda.Add("Gilsons");
+banda.Add("Maneva");
+banda.Add("Lagum");
+
+Console.WriteLine("As bandas adiconadas foram: ");
+foreach( string bandasAdc in banda)
+{
+    Console.WriteLine(bandasAdc);
+}
+Console.WriteLine("***********************");
+
+
+List<int> numeros = new List<int>();
+numeros.Add(16);
+numeros.Add(10);
+numeros.Add(20);
+numeros.Add(57);
+numeros.Add(12);
+
+int soma = 0;
+for(int i = 0; i < numeros.Count; i++)
+{
+    soma += numeros[i];
+}
+
+Console.WriteLine($"A soma dos números: {soma}");
